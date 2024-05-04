6756aebf468c60f20826d75e23f2672f1f99bbf9
AllanAgR <aaguilarr16@miumg.edu.gt>
2024-05-04 12:31:09 -0600

    subiendo archivos

---
+++ sprinboot-app-intellij/src/main/java/com/example/parcial/sprinbootapp/SprinbootAppIntellijApplication.java
@@ -0,0 +1,13 @@
+package com.example.parcial.sprinbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootAppIntellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootAppIntellijApplication.class, args);
+	}
+
+}
---
+++ sprinboot-app-intellij/src/main/java/com/example/parcial/sprinbootapp/course.java
@@ -0,0 +1,12 @@
+package com.example.parcial.sprinbootapp;
+
+public class course {
+        private int id;
+        private String name;
+
+
+}
+public course(int id, String name) {
+    this.id = id;
+    this.name = name;
+}
