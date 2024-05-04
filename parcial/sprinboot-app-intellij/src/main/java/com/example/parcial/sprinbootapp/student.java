db7af08b3ca0022e7a160c55462fab343ca02168
AllanAgR <106946946+AllanAgR@users.noreply.github.com>
2024-05-04 13:50:45 -0600

    Add files via upload

---
+++ parcial/sprinboot-app-intellij/src/main/java/com/example/parcial/sprinbootapp/student.java
@@ -0,0 +1,10 @@
+package com.example.parcial.sprinbootapp;
+
+public class student {
+    private int id;
+    private String name;
+}
+public student(int id, String name) {
+    this.id = id;
+    this.name = name;
+}
---
+++ parcial/sprinboot-app-intellij/src/test/java/com/example/parcial/sprinbootapp/SprinbootAppIntellijApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcial.sprinbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootAppIntellijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
